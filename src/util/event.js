425b9fa9a49219832358a7cc97723eaeec80dc9d
Andrew Danger Lyon <andrew@killtheradio.net>
2023-02-08 00:14:12 -0800

    event: on() returns unbinding fn, and adding a special handler event name :all: which gets triggered when any event is triggered

--- src/util/event.js
+++ src/util/event.js
@@ -1,9 +1,20 @@
 export default function Event() {
     const store = {};
+    const special_all_event = ':all:';
 
     const on = (evname, fn) => {
         if(!store[evname]) store[evname] = [];
         store[evname].push(fn);
+        // return a function that unbinds
+        return () => off(evname, fn);
+    };
+
+    const once = (evname, fn) => {
+        const unbind = on(evname, (...args) => {
+            fn(...args);
+            unbind();
+        });
+        return unbind;
     };
 
     const off = (evname, fn) => {
@@ -16,12 +27,17 @@ export default function Event() {
         }
     };
 
+    // use a queue so that events run in order of creation. in other words if
+    // event B is emitted while event A is taking place, all the handlers for
+    // event A will finish before calling handlers for event B.
     let running = false;
     let queue = [];
     const run_queue = () => {
         const [evname, args] = queue.shift();
         const handlers = (store[evname] || []).slice();
         handlers.forEach((fn) => fn(...args));
+        const all_handlers = (store[special_all_event] || []).slice();
+        all_handlers.forEach((fn) => fn(evname, ...args));
         if(queue.length === 0) return;
         run_queue();
     };
@@ -36,8 +52,10 @@ export default function Event() {
 
     return {
         on,
+        once,
         off,
         emit,
+        all: special_all_event,
     };
 };
 
